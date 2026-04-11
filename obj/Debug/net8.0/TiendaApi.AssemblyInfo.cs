ac49b7b2556cca4f3c8a3c95d40775f41e553dbb
victor <1918967@alu.murciaeduca.es>
2026-04-11 17:08:54 +0200

    arreglado para que se vean fotos y que se creen unicamente las variantes

--- obj/Debug/net8.0/TiendaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TiendaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0ebb53ca9b0e73cdbaf2718504e3024cb908bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e0b7204a6657403e33cff5db1895cf04d71983")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
