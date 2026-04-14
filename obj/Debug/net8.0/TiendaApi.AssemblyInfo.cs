cdd48ef8cfb3643691b214cdc673a2557ea500c4
victor <1918967@alu.murciaeduca.es>
2026-04-14 14:10:14 +0200

    nuevos cambios de visual en mis pedidos ya que no puedo porbarlo en localhost

--- obj/Debug/net8.0/TiendaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TiendaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9ab2f5f59265ebde09483113b9b012a6c8ba096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f83d4096fe114db18db0bdc9716daede082bcc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
